dfe1724f7699c43f2f9125fea08132af2222ebd5
wpsperson <wpsperson@163.com>
2023-06-25 13:42:56 +0800

    million triangles performance

--- QtOpenGLPractice/DemoOperation.cpp
+++ QtOpenGLPractice/DemoOperation.cpp
@@ -2,6 +2,7 @@
 #include "Renderer.h"
 #include "ProgramManager.h"
 #include "FontManager.h"
+#include "glad/glad.h"
 
 void injectAllOperation(Renderer* renderer)
 {
@@ -11,6 +12,10 @@ void injectAllOperation(Renderer* renderer)
     FontOperation* font = new FontOperation;
     font->setName("FontOperation");
     renderer->addOperation(font);
+    MillionPrimitiveOperation* million = new MillionPrimitiveOperation;
+    million->setName("MillionPrimitiveOperation");
+    renderer->addOperation(million);
+
 }
 
 FirstTriangle::~FirstTriangle()
@@ -70,4 +75,74 @@ void FontOperation::initialize(Renderer* renderer) noexcept
 void FontOperation::paint(Renderer* renderer) noexcept
 {
     renderer->paintFont(m_object);
+}
+
+MillionPrimitiveOperation::~MillionPrimitiveOperation()
+{
+}
+
+void MillionPrimitiveOperation::initialize(Renderer* renderer) noexcept
+{
+    glMatrixMode(GL_PROJECTION);
+    glLoadIdentity();
+    glOrtho(0, m_width, 0, m_height, -1, 1);
+}
+
+void MillionPrimitiveOperation::paint(Renderer* renderer) noexcept
+{
+    float advance = m_width / m_repeat_count;
+
+    Point triangle[3];
+    triangle[0].x = 0.0, triangle[0].y = 0;
+    triangle[1].x = advance, triangle[1].y = 0;
+    triangle[2].x = advance / 2, triangle[2].y = advance;
+    Point pt0{ 0.0, advance / 2 };
+    Point pt1{ advance, advance / 2 };
+
+    float xOffset = 0, yOffset = 0, zOffset = 0;
+    float xStep = advance;
+    float yStep = advance;
+
+    // batch strategy spend average 100ms, while retail strategy spend average 400ms
+    // same-state batch draw vs one by one draw:
+    // glBegin draw mode interleave: Triangles/Lines 100ms : 400ms
+    // glColor interleave: 150ms : 200 ms
+    // enable/disable line stipple 150ms : 1000ms
+    // enable line stipple, solid line/phantom line: 100ms/1000ms
+
+    glEnable(GL_LINE_STIPPLE);
+    if (m_batch_strategy)
+    {
+        glColor3f(0.5f, 0.5f, 0.5f);
+        glBegin(GL_TRIANGLES);
+        for (int i = 0; i < m_repeat_count; i++)
+        {
+            xOffset = i * xStep;
+            for (int j = 0; j < m_repeat_count; j++)
+            {
+                yOffset = j * yStep;
+                glVertex2f(triangle[0].x + xOffset, triangle[0].y + yOffset);
+                glVertex2f(triangle[1].x + xOffset, triangle[1].y + yOffset);
+                glVertex2f(triangle[2].x + xOffset, triangle[2].y + yOffset);
+            }
+        }
+        glEnd();
+    }
+    else
+    {
+        glBegin(GL_TRIANGLES);
+        for (int i = 0; i < m_repeat_count; i++)
+        {
+            xOffset = i * xStep;
+            glColor3f(i * 0.001f, 0.5f, 0.5f);
+            for (int j = 0; j < m_repeat_count; j++)
+            {
+                yOffset = j * yStep;
+                glVertex2f(triangle[0].x + xOffset, triangle[0].y + yOffset);
+                glVertex2f(triangle[1].x + xOffset, triangle[1].y + yOffset);
+                glVertex2f(triangle[2].x + xOffset, triangle[2].y + yOffset);
+            }
+        }
+        glEnd();
+    }
 }
--- QtOpenGLPractice/DemoOperation.h
+++ QtOpenGLPractice/DemoOperation.h
@@ -30,3 +30,20 @@ public:
 private:
     GLObject2D* m_object = nullptr;
 };
+
+class MillionPrimitiveOperation : public Operation
+{
+public:
+    ~MillionPrimitiveOperation();
+
+    virtual void initialize(Renderer* renderer) noexcept;
+
+    virtual void paint(Renderer* renderer) noexcept;
+
+private:
+    GLObject2D* m_object = nullptr;
+    double m_width = 1000.0;
+    double m_height = 1000.0;
+    int m_repeat_count = 1000;
+    bool m_batch_strategy = false;
+};
